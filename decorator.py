a9b51ccb0fd1e10485739b6e4e1182693614d06c
Sulav_Pandey <pandeysulav17@gmail.com>
2025-07-25 20:17:00 +0545

    Create decorator.py

---
+++ decorator.py
@@ -0,0 +1,8 @@
+# #Decorator = A function that extends the behavior of another function w/o modifying the base function.
+# Pass the base function as an argument to the decorator
+
+
+#         @add_sprinkles
+#         get_ice_cream("vanilla")
+
+
