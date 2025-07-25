b5f106f4ca7ecbf987c96c91d4907a89ff95cd30
Sulav_Pandey <pandeysulav17@gmail.com>
2025-07-25 20:14:57 +0545

    Update property_decorator_in_python.py

--- property_decorator_in_python.py
+++ property_decorator_in_python.py
@@ -2,51 +2,51 @@
 #         Benefit: Add additional logic when read, write, or delete attributes
 #         Gives you getter, setter and deleter method
 
-class Rectangle:
-    def __init__(self, width, height):
-        self._width = width
-        self._height = height
+# class Rectangle:
+#     def __init__(self, width, height):
+#         self._width = width
+#         self._height = height
 
-    @property
-    def width(self):
-        return f"{self._width:.1f}cm"
+#     @property
+#     def width(self):
+#         return f"{self._width:.1f}cm"
 
-    @property
-    def height(self):
-        return f"{self._height:.1f}cm"
+#     @property
+#     def height(self):
+#         return f"{self._height:.1f}cm"
     
-    @width.setter
-    def width(self, new_width):
-        if new_width > 0:
-            self._width = new_width
-        else:
-            print("Width must be greater than zero")    
+#     @width.setter
+#     def width(self, new_width):
+#         if new_width > 0:
+#             self._width = new_width
+#         else:
+#             print("Width must be greater than zero")    
 
-    @height.setter
-    def height(self, new_height):
-        if new_height > 0:
-            self._height = new_height  # ← fixed here
-        else:
-            print("Height must be greater than zero")  
+#     @height.setter
+#     def height(self, new_height):
+#         if new_height > 0:
+#             self._height = new_height  # ← fixed here
+#         else:
+#             print("Height must be greater than zero")  
 
-    @width.deleter
-    def width(self):
-        del self._width
-        print("Width has been deleted.")
+#     @width.deleter
+#     def width(self):
+#         del self._width
+#         print("Width has been deleted.")
 
-    @height.deleter
-    def height(self):
-        del self._height
-        print("Height has been deleted.")
+#     @height.deleter
+#     def height(self):
+#         del self._height
+#         print("Height has been deleted.")
 
 
-rectangle = Rectangle(5, 4)
+# rectangle = Rectangle(5, 4)
 
-rectangle.width = 5
-rectangle.height = 6
+# rectangle.width = 5
+# rectangle.height = 6
 
-del rectangle.width
-del rectangle.height
+# del rectangle.width
+# del rectangle.height
 
-print(rectangle.width)
-print(rectangle.height)
+# print(rectangle.width)
+# print(rectangle.height)
