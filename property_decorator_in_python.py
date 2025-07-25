047f026e69a5ce065d3223d50b57e0aa34dd4107
Sulav_Pandey <pandeysulav17@gmail.com>
2025-07-25 19:50:01 +0545

    Create property_decorator_in_python.py

---
+++ property_decorator_in_python.py
@@ -0,0 +1,4 @@
+# @property = Decorator used to define a method as a property (it can be accessed like an attri
+#         Benefit: Add additional logic when read, write, or delete attributes
+#         Gives you getter, setter and deleter method
+
