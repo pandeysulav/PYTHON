95be40c97bcc6651d24b3d9d932dafc0c1218efc
Sulav_Pandey <pandeysulav17@gmail.com>
2025-07-25 19:59:13 +0545

    Update property_decorator_in_python.py

--- property_decorator_in_python.py
+++ property_decorator_in_python.py
@@ -3,7 +3,19 @@
 #         Gives you getter, setter and deleter method
 
 class Rectangle:
-    def _init_(self, width, height):
-        self.width = width
-        self.height = height
+    def __init__(self, width, height):
+        self._width = width
+        self._height = height
 
+    @property
+    def width(self):
+        return f"{self._width:.1f}cm"
+
+    @property
+    def height(self):
+        return f"{self._height:.1f}cm"
+    
+rectangle = Rectangle(3, 4)
+
+print(rectangle.width)
+print(rectangle.height)
